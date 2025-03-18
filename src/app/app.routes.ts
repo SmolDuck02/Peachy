f2b3d70098d69b12a6eef576d07708af4d055ef9
Jameel Marco <londoholm1@gmail.com>
2025-03-18 21:55:00 +0800

    add route guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,48 @@
-import { Routes } from '@angular/router';
-import { AuthCallbackComponent } from './auth-callback/auth-callback.component';
+import { inject } from '@angular/core';
+import { Router, Routes } from '@angular/router';
+import { of, switchMap, take } from 'rxjs';
 import { AuthGuard } from './guards/auth.guard';
+import { AuthService } from './services/auth.service';
+import { DiscordAuthService } from './services/discord-auth.service';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  {
-    path: 'login',
-    loadComponent: () =>
-      import('./login/login.component').then((c) => c.LoginComponent),
-  },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   {
     path: 'dashboard',
     loadComponent: () =>
       import('./dashobard/dashobard.component').then(
         (c) => c.DashboardComponent
       ),
-    canActivate: [AuthGuard],
+    canMatch: [AuthGuard],
+  },
+  {
+    path: 'login',
+
+    canMatch: [
+      () => {
+        const authService = inject(AuthService);
+        const discordAuthService = inject(DiscordAuthService);
+        const router = inject(Router);
+        if (!discordAuthService.isAuthenticated()) {
+          return true;
+        }
+        router.navigateByUrl('/dashboard');
+        return false;
+        return authService.isLoggedIn().pipe(
+          take(1),
+          switchMap((isLoggedIn) => {
+            if (isLoggedIn || discordAuthService.isAuthenticated()) {
+              // Using navigateByUrl with replaceUrl to completely replace the current navigation
+              router.navigateByUrl('/dashboard');
+              return of(false);
+            }
+            return of(true);
+          })
+        );
+      },
+    ],
+    loadComponent: () =>
+      import('./login/login.component').then((c) => c.LoginComponent),
   },
   {
     path: 'auth/callback',
@@ -23,8 +50,7 @@ export const routes: Routes = [
       import('./auth-callback/auth-callback.component').then(
         (c) => c.AuthCallbackComponent
       ),
-      
   },
 
-  { path: '**', redirectTo: '/login' },
+  { path: '**', redirectTo: '/dashboard' },
 ];
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,30 +1,32 @@
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
 import { map, take } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
 import { DiscordAuthService } from '../services/discord-auth.service';
 
 export const AuthGuard = () => {
   const authService = inject(AuthService);
   const router = inject(Router);
-
   const discordAuthService = inject(DiscordAuthService);
-
 
   if (discordAuthService.isAuthenticated()) {
     return true;
   }
-  
+  router.navigate(['/login']);
+  return false;
   return authService.isLoggedIn().pipe(
     take(1),
     map((isLoggedIn) => {
-      if (isLoggedIn) {
-        return true;
-      } else {
+      setTimeout(() => {
+        if (isLoggedIn || discordAuthService.isAuthenticated()) {
+          return true;
+        }
         router.navigate(['/login']);
         return false;
-      }
+        // } else {
+        //   return false;
+        // }
+      }, 1000);
     })
   );
 };
-
