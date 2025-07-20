202c68fc178ab2e06cc9a10066c7e78a14c2630c
Jameel Marco <londoholm1@gmail.com>
2025-07-20 22:17:48 +0800

    update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,5 @@
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyDtv-Ogrhg1hzRweOtVG0zdH10OvPjwBnk',
-    authDomain: 'peachy-38736.firebaseapp.com',
-    projectId: 'peachy-38736',
-    storageBucket: 'peachy-38736.firebasestorage.app',
-    messagingSenderId: '997634715061',
-    appId: '1:997634715061:web:f47999e787f5216365bdfc',
-    measurementId: 'G-Y6N6RJWN2Q',
-  },
   API_ENDPOINT:"http://localhost:3000",
   REDIRECT_URI: 'http://localhost:4200',
 };
