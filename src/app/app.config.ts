aada95ae05cef299c7087f66b7f724387053460a
Jameel Marco <londoholm1@gmail.com>
2025-07-20 22:30:10 +0800

    remove firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,12 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
-import { environment } from '../environments/environment';
-
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { routes } from './app.routes';
+// provideFirebaseApp(() => initializeApp(environment.firebase))
 export const appConfig: ApplicationConfig = {
-  providers: [provideFirebaseApp(() => initializeApp(environment.firebase)),
+  providers: [
       provideAuth(() => getAuth()), provideHttpClient(withFetch()), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
 };
